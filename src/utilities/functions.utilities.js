e5aa83a9055814409bac693359c5491302607ac9
granddivino <granddivino@gmail.com>
2020-12-22 21:32:45 -0700

    Combined auth

--- src/utilities/functions.utilities.js
+++
@@ -1,9 +0,0 @@
-//Takes res error and return it into a readable format
-export const resMessage = (error) => {
-    return (error.response && 
-        error.response.data && 
-        error.response.data.message) ||
-        error.message ||
-        error.toString()
-}
-
